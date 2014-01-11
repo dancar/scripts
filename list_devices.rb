e4a8c3079352fda1e03cd0710ce4c3b03b7dc8cd
Dan Carmon <dan.carmon@trusteer.com>
2014-01-11 03:18:42 +0200

    show first name argument

--- list_devices.rb
+++ list_devices.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+# Scrape devices list out of a RangeMax Next Wireless-N Modem Router (DGN2200) web interface
 require 'net/http'
 require 'uri'
 require 'terminal-table'
@@ -8,16 +9,29 @@ require 'nokogiri'
 uri = URI.parse("http://10.0.0.138:80/devices.cgi")
 http = Net::HTTP.new(uri.host, uri.port)
 request = Net::HTTP::Post.new(uri.request_uri)
-password = File.read("~/rt.pw")
+password = File.read(File.expand_path("~/rt.pw")).chomp
 username = "Admin"
 request.basic_auth(username, password)
 request.set_form_data(refresh: "Refresh")
 response = http.request(request)
 doc = Nokogiri::XML(response.body)
 rows = doc.css(".ttext")
-terminal_rows = (rows.length / 3).times.map do |i|
-  3.times.map do |j|
+devices = (rows.length / 3).times.map do |i|
+  arr = 3.times.map do |j|
     rows[(i * 3) + j].children.first.text
   end
+  {
+    ip: arr[0],
+    name: arr[1],
+    mac: arr[2]
+  }
 end
-puts Terminal::Table.new rows: terminal_rows
+
+name = ARGV[0]
+if name
+  devices.each do |device|
+    puts device[:ip] if device[:name].casecmp(name) == 0
+  end
+else
+  puts Terminal::Table.new rows: devices.map(&:values)
+end
